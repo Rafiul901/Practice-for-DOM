476358f947de2ce0e717bfbc88f66fe9138fe754
Rafiul901 <rafiuldipu2480@gmail.com>
2025-11-17 20:23:54 +0600

    new

--- script.js
+++ script.js
@@ -47,4 +47,15 @@ function longestWord(sentence){
     }
     return maxWord;
 }
-console.log(longestWord('I love programming'));
+// console.log(longestWord('I love programming'));
+
+//6
+
+function firstNonRepeating(str){
+    for(let i=0; i<str.length; i++){
+        if(str.indexOf(str[i])=== str.lastIndexOf(str[i]))
+            return str[i]
+    }
+    return null;
+}
+console.log(firstNonRepeating('swiss'));
