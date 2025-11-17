bd9d64bdd21ed43275ed21c70fbe4ba51ed2354a
Rafiul901 <rafiuldipu2480@gmail.com>
2025-11-17 21:53:22 +0600

    soved number seven

--- script.js
+++ script.js
@@ -58,4 +58,12 @@ function firstNonRepeating(str){
     }
     return null;
 }
-console.log(firstNonRepeating('swiss'));
+// console.log(firstNonRepeating('swiss'));
+
+//7
+function isAnagram(s1,s2){
+    let sorted1 =s1.split('').sort().join('');
+    let sorted2 =s2.split('').sort().join('');
+    return sorted1 === sorted2;
+}
+console.log(isAnagram('listen','silent'));
