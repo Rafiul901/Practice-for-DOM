1a6477a8c5e10aaf96f83f8e57fb599a9a58c414
Rafiul901 <rafiuldipu2480@gmail.com>
2025-11-23 19:10:37 +0600

    new

--- script.js
+++ script.js
@@ -135,4 +135,13 @@ function sumArray(arr){
 function removeDuplicates (arr){
     return [...new Set(arr)];
 }
-console.log(removeDuplicates([1,3,5,6,6,2]));
+// console.log(removeDuplicates([1,3,5,6,6,2]));
+
+function countEvenOdd(arr){
+    let even =0; odd =0;
+    for(let num of arr){
+        if(num%2===0) even++; else odd++;
+    }
+    return{even,odd}
+}
+console.log(countEvenOdd([1,2,3,4,5,6]));
