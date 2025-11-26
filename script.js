903aee3a5200d8ebe8a810651cd1a2e18344433a
Rafiul901 <rafiuldipu2480@gmail.com>
2025-11-26 22:10:30 +0600

    new

--- script.js
+++ script.js
@@ -147,3 +147,7 @@ function countEvenOdd(arr){
  console.log(countEvenOdd([1,2,3,4,4,5,6]));
 
 //18
+function average (arr){
+    return arr.reduce((a,b)=> a+b,0)/arr.length;
+}
+console.log(average([1,2,3,4,5,6]));
